61095b4c7fcf1bf16d5b14da0c0e36ce143b8584
Chris Waters <chris.isaac.waters@gmail.com>
2019-12-15 18:54:42 -0500

    Build api

---
+++ Controllers/Resources/ModelResource.cs
@@ -0,0 +1,11 @@
+using VEGA.Models;
+
+namespace VEGA.Controllers.Resources
+{
+    public class ModelResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
