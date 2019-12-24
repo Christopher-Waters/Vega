512b7a12a5e3851a3bea335ecd25ad56bd408f50
Chris Waters <chris.isaac.waters@gmail.com>
2019-12-24 10:01:35 -0500

    Extending the domain model

---
+++ ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,30 @@
+import { VehicleService } from '../../services/vehicle.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes: any = [];
+  models: any[];
+  vehicle: any = {};
+  features: any = [];
+  constructor(
+    private vehicleService: VehicleService) { }
+
+  ngOnInit() {
+    this.vehicleService.getMakes().subscribe(makes =>
+      this.makes = makes);
+    this.vehicleService.getFeatures().subscribe(features =>
+      this.features = features)  ;
+  }
+
+  onMakeChange() {
+      // tslint:disable-next-line: triple-equals
+      const selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+      this.models = selectedMake ? selectedMake.models : [];
+  }
+
+}
